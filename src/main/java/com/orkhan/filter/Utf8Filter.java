17657411c804f85185c89699e54b345f449fe2af
Orkhan Mamedov <orx.mamedov@gmail.com>
2023-12-02 21:46:20 +0400

    added utf 8 filer

---
+++ src/main/java/com/orkhan/filter/Utf8Filter.java
@@ -0,0 +1,32 @@
+package com.orkhan.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class Utf8Filter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code if needed
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // Set character encoding to UTF-8 for both request and response
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        // Continue the chain
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code if needed
+    }
+}
